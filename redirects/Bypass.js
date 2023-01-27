272b48c007a8711c07ae50b09815a97a68ae7482
BielPlugner <90593822+BielPlugner@users.noreply.github.com>
2023-01-26 22:52:14 -0300

    Update Bypass.js

--- redirects/Bypass.js
+++ redirects/Bypass.js
@@ -3,21 +3,16 @@
     let captcha3 = 'https://linkvertise.com/48193/thekrnlkey/1'
     let captcha4 = 'https://linkvertise.com/48193/krnlc3/1'
 
-    //let urlatual = window.location.href;
 
-
-    if (urlatual == captcha1) {
+    if (window.location.href.includes(captcha1)) {
         window.location.href = 'https://texturas3dff.com/2-captcha-krnl/'
     }
-    else if(urlatual == captcha2){
+    else if (window.location.href.includes(captcha2)){
         window.location.href = 'https://texturas3dff.com/3-captcha-krnl/'
     }
-    else if(urlatual == captcha3){
+    else if (window.location.href.includes(captcha3)){
         window.location.href = 'https://texturas3dff.com/4-captcha-krnl/'
     }
-    else if(urlatual == captcha4){
+    else if (window.location.href.includes(captcha4)){
         window.location.href = 'https://texturas3dff.com/5-captcha-krnl/'
-    }
-    else {
-        console.log('Nenhuma delas!')
     }
