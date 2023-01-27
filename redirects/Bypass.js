42ec122c837557e0e5b4334580d7c0f2fd73079f
BielPlugner <90593822+BielPlugner@users.noreply.github.com>
2023-01-26 21:50:15 -0300

    Create Bypass.js

---
+++ redirects/Bypass.js
@@ -0,0 +1,20 @@
+    let captcha1 = 'https://linkvertise.com/48193/krnlc4/1'
+    let captcha2 = 'https://linkvertise.com/48193/krnlkey/1'
+    let captcha3 = 'https://linkvertise.com/48193/thekrnlkey/1'
+    let captcha4 = 'https://linkvertise.com/48193/krnlc3/1'
+
+    let urlatual = window.location.href;
+
+
+    if (urlatual == captcha1) {
+        window.location.href = 'https://texturas3dff.com/2-captcha-krnl/'
+    }
+    else if(urlatual == captcha2){
+        window.location.href = 'https://texturas3dff.com/3-captcha-krnl/'
+    }
+    else if(urlatual == captcha3){
+        window.location.href = 'https://texturas3dff.com/4-captcha-krnl/'
+    }
+    else if(urlatual == captcha4){
+        window.location.href = 'https://texturas3dff.com/5-captcha-krnl/'
+    }
