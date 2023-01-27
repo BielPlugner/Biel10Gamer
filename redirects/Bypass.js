750df9b19094529fcf24f55e6e1b0065189b5430
BielPlugner <90593822+BielPlugner@users.noreply.github.com>
2023-01-27 04:13:26 -0300

    Update Bypass.js

--- redirects/Bypass.js
+++ redirects/Bypass.js
@@ -25,24 +25,3 @@ if (window.location.href.includes("example.com/captcha2")) {
 }
 
 
-//     let captcha1 = 'https://linkvertise.com/48193/krnlc4/1'
-//     let captcha2 = 'https://linkvertise.com/48193/krnlkey/1'
-//     let captcha3 = 'https://linkvertise.com/48193/thekrnlkey/1'
-//     let captcha4 = 'https://linkvertise.com/48193/krnlc3/1'
-
-
-//     if (window.location.href.includes(captcha1)) {
-//         window.location.href = 'https://texturas3dff.com/2-captcha-krnl/'
-//     }
-//     else if (window.location.href.includes(captcha2)){
-//         window.location.href = 'https://texturas3dff.com/3-captcha-krnl/'
-//     }
-//     else if (window.location.href.includes(captcha3)){
-//         window.location.href = 'https://texturas3dff.com/4-captcha-krnl/'
-//     }
-//     else if (window.location.href.includes(captcha4)){
-//         window.location.href = 'https://texturas3dff.com/5-captcha-krnl/'
-//     }
-//     else{
-//     console.log('Olá acho que n deu nenhum:)
-//     }
